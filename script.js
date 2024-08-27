cac4187496871aff5091d9abf1c6ef5bc7eaf3a9
ArabyMovies <74076843+ArabyMovies@users.noreply.github.com>
2024-08-27 13:52:45 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("userForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    let nom = document.getElementById("nom").value;
+    let prenom = document.getElementById("prenom").value;
+
+    // Création de l'objet FormData pour envoyer les données
+    let formData = new FormData();
+    formData.append("nom", nom);
+    formData.append("prenom", prenom);
+
+    fetch("http://test.com/ajout.php", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        let messageDiv = document.getElementById("message");
+        if (data.success) {
+            messageDiv.innerHTML = "<p style='color: green;'>Utilisateur ajouté avec succès.</p>";
+        } else {
+            messageDiv.innerHTML = "<p style='color: red;'>Erreur: " + data.message + "</p>";
+        }
+    })
+    .catch(error => {
+        console.error('Erreur:', error);
+        document.getElementById("message").innerHTML = "<p style='color: red;'>Une erreur est survenue lors de l'ajout de l'utilisateur.</p>";
+    });
+});
