c92c2284c4a5224860b76bc7f8e5de5eab695c28
ArabyMovies <74076843+ArabyMovies@users.noreply.github.com>
2024-08-29 10:35:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', (event) => {
     const nom = document.getElementById('nom').value;
     const prenom = document.getElementById('prenom').value;
 
-    fetch('https://www.wkjimvbsxr.wuaze.com/ajout.php', {
+    fetch('https://download1586.mediafire.com/zv1iuub0ok8gAZgwr1H2ktxDCpiYG_k9pLEU2pXNObYVrNldxsFZAzlTviNRTaKf6c1UhP4y-IHM3BfnBOl1vXoVQgco2ttybDwO20UegH2pJrgJ-kcSdsIElOC15ns90T_ImqtrGu0_1I1Vl9uPfVfAc19BzUffc2i97uGeKuxEa3o/z0wgd02nem406x0/ajout.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
