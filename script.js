0dcdb21b8bb7d7ef3757fb31e5badac369a6f4f9
ArabyMovies <74076843+ArabyMovies@users.noreply.github.com>
2024-09-21 13:57:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
-const port = 3000; // Choisissez un port de votre choix
+const port = 3306; // Choisissez un port de votre choix
 
 // Middleware
 app.use(cors());
